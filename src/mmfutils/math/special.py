dd9b88a203d8355f954247d84bb36ffb9931ee49
Michael McNeil Forbes <michael.forbes+python@gmail.com>
2024-07-05 12:55:40 -1000

    ENH: Added d=1 option to step and mstep.  (Needs tests)
    --HG--
    branch : 0.6

--- src/mmfutils/math/special.py
+++ src/mmfutils/math/special.py
@@ -43,31 +43,92 @@ def ellipkinv(K, iter=4):
     return m
 
 
-def step(t, t1, alpha=3.0):
+def step(t, t1, alpha=3.0, d=0):
     r"""Smooth step function that goes from 0 at time ``t=0`` to 1 at time
     ``t=t1``.  This step function is $C_\infty$:
+
+    Arguments
+    ---------
+    t : float
+        Independent variable.
+    t1 : float
+        Parameter defining length of step (from 0 to t1).
+    alpha : float
+        Shape parameter.  Larger values are steeper.
+    d : [0, 1]:
+        Return the d'th derivative.
     """
     if t < 0.0:
         return 0.0
     elif t < t1:
-        return (1 + math.tanh(alpha * math.tan(math.pi * (2 * t / t1 - 1) / 2))) / 2
+        if d == 0:
+            return (1 + math.tanh(alpha * math.tan(math.pi * (2 * t / t1 - 1) / 2))) / 2
+        elif d == 1:
+            # This has overflow issues...
+            theta = math.pi * t / t1
+            try:
+                return (math.pi * alpha) / (
+                    (2 * t1)
+                    * (math.sin(theta) * math.cosh(alpha / math.tan(theta))) ** 2
+                )
+            except OverflowError:
+                return 0.0
+        else:
+            raise NotImplementedError(f"Only d=0 or d=1 supported (got {d=})")
     else:
-        return 1.0
+        if d == 0:
+            return 1.0
+        else:
+            return 0.0
 
 
-def mstep(t, t1, alpha=3.0):
+def mstep(t, t1, alpha=3.0, d=0):
     r"""Smooth step function that goes from 0 at time ``t=0`` to 1 at time
     ``t=t1``. This step function is $C_\infty$:
 
-    This is a vectorized version of `step()` using np.piecewi.
+    This is a vectorized version of `step()` using np.piecewise.
+
+    Arguments
+    ---------
+    t : array-like
+        Independent variable.
+    t1 : float
+        Parameter defining length of step (from 0 to t1).
+    alpha : float
+        Shape parameter.  Larger values are steeper.
+    d : [0, 1]:
+        Return the d'th derivative.
     """
     t = np.asarray(t)
-    return np.piecewise(
-        t,
-        [t < 0.0, np.logical_and(0 <= t, t < t1)],
-        [
-            0.0,
-            lambda t: (1 + np.tanh(alpha * np.tan(np.pi * (2 * t / t1 - 1) / 2))) / 2,
-            1.0,
-        ],
-    )
+    theta = np.pi * t / t1
+    if d == 0:
+        return np.piecewise(
+            theta,
+            [theta < 0.0, np.logical_and(0 <= theta, theta < np.pi)],
+            [
+                0.0,
+                lambda theta: (1 - np.tanh(alpha / np.tan(theta))) / 2,
+                1.0,
+            ],
+        )
+    elif d == 1:
+        with np.errstate(over="ignore"):
+            # The cosh overflows here for small t, but this is not an issue since
+            # the result is zero.
+            return np.piecewise(
+                theta,
+                [theta < 0.0, np.logical_and(0 <= theta, theta < np.pi)],
+                [
+                    0.0,
+                    lambda theta: (
+                        (np.pi * alpha)
+                        / (
+                            (2 * t1)
+                            * (np.sin(theta) * np.cosh(alpha / np.tan(theta))) ** 2
+                        )
+                    ),
+                    0.0,
+                ],
+            )
+    else:
+        raise NotImplementedError(f"Only d=0 or d=1 supported (got {d=})")
