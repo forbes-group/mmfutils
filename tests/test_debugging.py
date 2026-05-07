885a2d06133a9018adea739b4ab6161ea4f9f62a
Michael McNeil Forbes <michael.forbes+python@gmail.com>
2026-05-07 14:43:54 -0700

    BUG,TST: Demonstrate issue #41 -- debug fails with generators.

--- tests/test_debugging.py
+++ tests/test_debugging.py
@@ -41,3 +41,16 @@ class TestCoverage(object):
         env = {}
         with pytest.raises(ValueError):
             debug(f, env, 3)()
+
+    def test_generator(self):
+        """Test generator expressions: see issue #41."""
+        env = {}
+
+        @debug(env)
+        def gen(x):
+            y = x**2
+            yield y
+
+        list(gen(2))
+        assert env["x"] == 2
+        assert env["y"] == 4
